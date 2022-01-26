800338998cc7b97dcdfc6a097ddba2d3baee877d
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2022-01-25 22:14:56 -0300

    menu criado e banco de palavras configurado.

---
+++ jogo_forca.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+print("Bem vindo ao jogo da forca!")
+
+menu = int
+palavras = ["EMPURRE", "ARQUEOLOGIA", "ESPAGUETE", "JARDIM", "ASTRONAUTA", "CINTURA", "SEMEADURA", "UTILIDADE", "LIVRO", "CICRATIZAÇAO"]
+
+def sortear_palavra(palavras):
+        i = randint(0, (len(palavras) - 1))
+        print(palavras[i])
+
+while menu != 2:
+    menu = int(input("Para jogar digite: 1\nPara sair digite: 2\n"))
+    sortear_palavra(palavras)
+
+
+
+exit()
